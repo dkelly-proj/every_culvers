a99bd47042357ec6e2ead814090f1b7675ba7e03
Dustin <dk460110@gmail.com>
2024-08-29 16:33:37 +0000

    automating data refresh

---
+++ update_and_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ~/every_culvers/ec_env/bin/activate
+
+# Navigate to the project directory
+cd ~/every_culvers
+
+# Run the data retrieval script
+python3 culvers_locations_retrieval.py
+
+# Run the map builder script
+python3 culvers_map_builder.py
+
+# Add changes to git
+git add culvers_locations_map.html
+git commit -m "Daily update: $(date +'%Y-%m-%d')"
+
+# Push the changes to GitHub
+git push origin main
+
+# Deactivate the virtual environment
+deactivate
