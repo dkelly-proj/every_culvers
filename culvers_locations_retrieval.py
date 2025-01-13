bf4eb897319367ddb1e4e17ed3baaa93aee88536
Dustin <dk460110@gmail.com>
2025-01-13 17:07:55 -0500

    updating states

--- culvers_locations_retrieval.py
+++ culvers_locations_retrieval.py
@@ -15,9 +15,9 @@ us_states = [
     "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", 
     "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", 
     "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", 
-    "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", 
+    "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "NC", 
     "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", 
-    "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
+    "South Carolina", "SC", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
     "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
 ]
 
